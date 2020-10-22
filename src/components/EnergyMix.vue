9b8708cfbf5de8a0d08a665fbad5e4b096533ac2
JenkenK <jenkenkwok@gmail.com>
2020-10-22 15:43:36 +0100

    removed unecessary h2 in energymix.vue

--- src/components/EnergyMix.vue
+++ src/components/EnergyMix.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-      <h2></h2>
       <ul v-if="fuelData">
           <li v-for="(energy, index) in fuelData" :energy="energy" :key="index">
               <h2>{{ energy.fuel.toUpperCase() }}</h2>
