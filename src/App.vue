7939f8dcb17faff17d14ac12263eaf1a6da20d44
JenkenK <jenkenkwok@gmail.com>
2020-10-22 11:29:45 +0100

    created app.vue and energy mix component, displays each fuel type and percentage in energy mix component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
-<template>
-  
+<template lang='html'>
+	<div>
+		<h1>Energy Tracker</h1>
+		<energy-list :fuelData='fuelData'></energy-list>
+	</div>
 </template>
 
 <script>
-export default {
+import EnergyMix from './components/EnergyMix.vue';
 
+export default {
+	name: 'app',
+	data(){
+		return {
+			fuelData: []
+		}
+	},
+	mounted () {
+        fetch ('https://api.carbonintensity.org.uk/generation')
+        .then (response => response.json())
+        .then (json => this.fuelData = json.data.generationmix);
+	}, 
+	components:{
+		"energy-list" : EnergyMix
+	}
 }
+
 </script>
 
-<style>
+<style lang="css" scoped>
 
 </style>
---
+++ src/components/EnergyMix.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <ul v-if="fuelData">
+          <li v-for="(energy, index) in fuelData" :energy="energy" :key="index">
+              <h2>{{ energy.fuel }}</h2>
+              <p>{{ energy.perc }} %</p>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'energy-list',
+    props: ['fuelData'],
+}
+</script>
+
+<style lang="css" scoped>
+li{
+    list-style-type: none;
+}
+</style>
