209ecee33d89d5cddea29ed980a182fd5c1f3009
JenkenK <jenkenkwok@gmail.com>
2020-10-22 12:29:26 +0100

    added GCharts

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,37 @@
 	<div>
 		<h1>Energy Tracker</h1>
 		<energy-list :fuelData='fuelData'></energy-list>
+
+		<GChart
+			type="Col"
+			:data="chartData"
+			:options="chartOptions"
+  		/>
 	</div>
 </template>
 
 <script>
 import EnergyMix from './components/EnergyMix.vue';
+import { GChart } from 'vue-google-charts'
 
 export default {
 	name: 'app',
 	data(){
 		return {
-			fuelData: []
+			fuelData: [], 
+			chartData: [
+				['Year', 'Sales', 'Expenses', 'Profit'],
+				['2014', 1000, 400, 200],
+				['2015', 1170, 460, 250],
+				['2016', 660, 1120, 300],
+				['2017', 1030, 540, 350]
+			],
+			chartOptions:{
+				chart:{
+					title: 'Company Performance',
+          			subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+				}
+			}
 		}
 	},
 	mounted () {
@@ -21,7 +41,8 @@ export default {
         .then (json => this.fuelData = json.data.generationmix);
 	}, 
 	components:{
-		"energy-list" : EnergyMix
+		"energy-list" : EnergyMix,
+		GChart
 	}
 }
 
