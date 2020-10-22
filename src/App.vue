01652bc398a601f1885ae0a8ea54181b73ba12da
JenkenK <jenkenkwok@gmail.com>
2020-10-22 12:40:16 +0100

    added hard coded data to make pie chart working

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 		<energy-list :fuelData='fuelData'></energy-list>
 
 		<GChart
-			type="Col"
+			type="PieChart"
 			:data="chartData"
 			:options="chartOptions"
   		/>
@@ -21,11 +21,10 @@ export default {
 		return {
 			fuelData: [], 
 			chartData: [
-				['Year', 'Sales', 'Expenses', 'Profit'],
-				['2014', 1000, 400, 200],
-				['2015', 1170, 460, 250],
-				['2016', 660, 1120, 300],
-				['2017', 1030, 540, 350]
+				['Fuel Type', 'Percentage'],
+				['Biomass', 20],
+				['Wind', 30],
+				['Gas', 50]
 			],
 			chartOptions:{
 				chart:{
